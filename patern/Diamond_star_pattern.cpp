dcfdcd3f99a39f639a56cd21b16b2702bb4b3a38
shriman keshri <shrimansoft@github.com>
2018-09-07 10:19:18 +0530

    this is my petern

---
+++ patern/Diamond_star_pattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+cout << argc<< endl;
+
+for( int i=0;i < argcali;i++)
+cout << argv[i]<< endl ;
+
+
+
+
+  return 0;
+}
